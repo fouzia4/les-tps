7af210de74811f34b9ef2a4adc93faff345dce8a
fouzia4 <outmanifouzia4@gmail.com>
2025-04-15 23:03:14 +0100

    Add files via upload

---
+++ exercice1.js
@@ -0,0 +1,6 @@
+let a = parseFloat(prompt("saisir le premier nombre :"));
+let b = parseFloat(prompt("saisir le deuxième nombre :"));
+ console.log("Somme : " + (a+b));
+  console.log("Différence : " + (a-b));
+  console.log("Produit : " +(a*b));
+  console.log("Quotient : " + (a+b));
